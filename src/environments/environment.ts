f93a21ee23133ba2ab09a71ba80df08a1ecde31f
Yash Sawlani <yash.sawlani@ibm.com>
2024-10-24 16:43:48 +0530

    vars updated

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  apiBaseUrl: 'http://localhost:5000',
+  baseUrl: 'PLACEHOLDER_baseUrl',
+  clientId: 'PLACEHOLDER_clientId',
+  apiClientId: 'PLACEHOLDER_apiClientId',
+  clientSecret: 'PLACEHOLDER_clientSecret',
+  apiClientSecret: 'PLACEHOLDER_apiClientSecret',
+  CE_URL: 'PLACEHOLDER_CE_URL',
+  scope: 'openid',
+};
